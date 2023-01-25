c1a678b6bc8919589d882a679bb9fcbc70e658e4
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-01-25 10:06:37 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.38 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        int[] az = new int[26];
+        for (int i = 0; i < 26; i++) {
+            az[i] = -1;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if(az[(int)s.charAt(i)-97] == -1){
+                answer[i] = -1;
+                System.out.println(answer[i]);
+            }else{
+                answer[i] = i-az[(int)s.charAt(i)-97];
+            }
+            az[(int)s.charAt(i)-97] = i;
+        }
+        return answer;
+    }
+}
