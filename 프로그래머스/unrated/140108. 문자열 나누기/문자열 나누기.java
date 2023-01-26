0abdcabee3299fd4f5ad7c4eb6323c025cbca4e9
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-01-26 09:40:56 +0900

    [unrated] Title: 문자열 나누기, Time: 1.02 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.java
@@ -1,31 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int solution(String s) {
         int answer = 0;
         int cnt = 0;
-        List<StringBuilder> stringList = new ArrayList<>();
+//        List<StringBuilder> stringList = new ArrayList<>();
         char a = s.charAt(0);
-        StringBuilder string = new StringBuilder();
+//        StringBuilder string = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == a){
-                string.append(s.charAt(i));
+//                string.append(s.charAt(i));
                 cnt++;
             }else{
-                string.append(s.charAt(i));
+//                string.append(s.charAt(i));
                 cnt--;
             }
             if(i == s.length()-1){
-                stringList.add(string);
+//                stringList.add(string);
+                answer++;
                 break;
             }
             if(cnt == 0){
-                stringList.add(string);
+//                stringList.add(string);
+                answer++;
                 a = s.charAt(i+1);
             }
         }
-        answer = stringList.size();
         return answer;
     }
 }
