497c0bd813827611746bc98a85a9fddd89490ce4
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-01-29 00:04:28 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.29 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,26 +1,16 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        List<Integer> list = new ArrayList<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        int temp = 0;
         for (int i = 0; i < score.length; i++) {
-            if(list.size() < k){
-                list.add(score[i]);
-                list.sort(Collections.reverseOrder());
-                answer[i] = list.get(list.size()-1);
-            }else{
-                if(list.get(list.size()-1) < score[i]){
-                    list.set(list.size()-1,score[i]);
-                    if(list.get(list.size()-2) < list.get(list.size()-1)) {
-                        list.sort(Collections.reverseOrder());
-                    }
-                }
-                answer[i] = list.get(list.size()-1);
+            priorityQueue.add(score[i]);
+            if(priorityQueue.size() > k){
+                priorityQueue.poll();
             }
+            answer[i] = priorityQueue.peek();
         }
         return answer;
     }
