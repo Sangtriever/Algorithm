06a933f6241b9b92958b96c9eb10b4bab432ee06
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-04 23:31:10 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.33 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -4,11 +4,10 @@ class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        int temp = 0;
         for (int i = 0; i < score.length; i++) {
             priorityQueue.add(score[i]);
             if(priorityQueue.size() > k){
-                priorityQueue.poll();
+                priorityQueue.remove();
             }
             answer[i] = priorityQueue.peek();
         }
