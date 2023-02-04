c63bf4bc45ef750df9baf5e2015c7865ecd695e3
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-04 23:17:23 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.17 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,19 +1,19 @@
+
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        for (int i = 1; i < number+1; i++) {
-            int cnt = 0;
-            for (int j = 1; j * j <= i; j++) {
-                if(j * j == i){
-                    cnt++;
-                }else if(i % j == 0){
-                    cnt += 2;
-                }
+        int[] count = new int[number + 1];    
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            if(cnt > limit){
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
                 answer += power;
-            }else{
-                answer += cnt;
+            } else {
+                answer += count[i];
             }
         }
         return answer;
