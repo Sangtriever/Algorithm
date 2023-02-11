f00bda7ca2ae3613525edd7fa2f04caeef3d516d
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-11 21:04:29 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.25 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] food) {
+        String result = "";
+        StringBuilder answer = new StringBuilder();
+        for (int i = 1; i < food.length; i++) {
+            answer.append(String.valueOf(i).repeat(Math.max(0, food[i] / 2)));
+            System.out.println();
+        }
+        answer.append(0);
+        for (int i = food.length-1; i >= 1 ; i--) {
+            answer.append(String.valueOf(i).repeat(Math.max(0, food[i] / 2)));
+        }
+        result = String.valueOf(answer);
+        return answer.toString();
+    }
+}
