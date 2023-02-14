0e173f715cb3efa7839ec029fc4f067b1351ee88
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-14 13:37:36 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.13 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String X, String Y) {
+        StringBuilder answer = new StringBuilder();
+        int[] list1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] list2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        for (int i = 9; i >= 0; i--) {
+            list1[i] = X.length() - X.replace(String.valueOf(i),"").length();
+            list2[i] = Y.length() - Y.replace(String.valueOf(i),"").length();
+            int num = Math.min(list1[i],list2[i]);
+            if(num != 0){
+                for (int j = 0; j < num; j++) {
+                    answer.append(String.valueOf(i));
+                }
+            }
+        }
+        String result;
+        if (answer.toString().equals("")) {
+            return "-1";
+        } else if (answer.toString().replace("0","").equals("")) {
+            return "0";
+        } else {
+            return answer.toString();
+        }
+    }
+}
