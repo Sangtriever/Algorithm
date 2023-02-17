884e4d205a9491afa826191038fb7c9c8e67cba5
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-17 23:18:11 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.77 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        int todays = dayCur(today);
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < terms.length; i++) {
+            hashMap.put(terms[i].charAt(0),Integer.parseInt(terms[i].substring(2))*28);
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < privacies.length; i++) {
+            int a = hashMap.get(privacies[i].charAt(11));
+            int day = dayCur(privacies[i])+ a;
+            if(todays >= day){
+                list.add(i+1);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+    public int dayCur(String days){
+        int year = 336 * Integer.parseInt(days.substring(0,4));
+        int month = 28 * Integer.parseInt(days.substring(5,7));
+        int day = Integer.parseInt(days.substring(8,10));
+        return year + month + day;
+    }
+}
