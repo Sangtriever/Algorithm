f7d2c7a6d7f4c9a6d058134c24d2ad037e658ccd
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-21 22:57:59 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.74 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -3,24 +3,24 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
+    public ArrayList<Integer> solution(String today, String[] terms, String[] privacies) {
         int todays = dayCur(today);
         HashMap<Character, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < terms.length; i++) {
             hashMap.put(terms[i].charAt(0),Integer.parseInt(terms[i].substring(2))*28);
         }
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         for (int i = 0; i < privacies.length; i++) {
             int a = hashMap.get(privacies[i].charAt(11));
             int day = dayCur(privacies[i])+ a;
             if(todays >= day){
-                list.add(i+1);
+                answer.add(i+1);
             }
         }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        // int[] answer = new int[list.size()];
+        // for (int i = 0; i < list.size(); i++) {
+        //     answer[i] = list.get(i);
+        // }
         return answer;
     }
     public int dayCur(String days){
