0b5758d9de69c618c84b877dd824e1f4fdc587aa
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-02-28 15:24:35 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.39 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,22 +1,27 @@
 import java.util.Arrays;
 
-public class Solution {
-    public static String[] solution(int n, int[] arr1, int[] arr2) {
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[arr1.length];
+        String[] arr1BinaryString = new String[arr1.length];
+        String[] arr2BinaryString = new String[arr2.length];
+        String[] resultBinaryString = new String[arr1.length];
         for (int i = 0; i < arr1.length; i++) {
-            String arr1BinaryString = Integer.toBinaryString(arr1[i]);
-            String arr2BinaryString = Integer.toBinaryString(arr2[i]);
-
-            arr1BinaryString = String.format("%" + n + "s", arr1BinaryString).replace(" ", "0");
-            arr2BinaryString = String.format("%" + n + "s", arr2BinaryString).replace(" ", "0");
-
-            char[] charlist1 = arr1BinaryString.toCharArray();
-            char[] charlist2 = arr2BinaryString.toCharArray();
+            arr1BinaryString[i] = Integer.toBinaryString(arr1[i]);
+            arr2BinaryString[i] = Integer.toBinaryString(arr2[i]);
+            while (arr1BinaryString[i].length() < arr1.length){
+                arr1BinaryString[i] = "0" + arr1BinaryString[i];
+            }
+            while (arr2BinaryString[i].length() < arr1.length){
+                arr2BinaryString[i] = "0" + arr2BinaryString[i];
+            }
+            char[] charlist1 = arr1BinaryString[i].toCharArray();
+            char[] charlist2 = arr2BinaryString[i].toCharArray();
             answer[i] = "";
             for (int j = 0; j < charlist1.length; j++) {
                 if (charlist1[j] == '1' || charlist2[j] == '1') {
                     answer[i] += "#";
-                } else {
+                }else {
                     answer[i] += " ";
                 }
             }
