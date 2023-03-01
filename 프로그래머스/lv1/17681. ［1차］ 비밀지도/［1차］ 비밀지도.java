7995013143b30eda5fa97b48ca05e82ec915a1cd
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-03-01 21:43:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.13 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -3,20 +3,17 @@ import java.util.Arrays;
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[arr1.length];
-        String[] arr1BinaryString = new String[arr1.length];
-        String[] arr2BinaryString = new String[arr2.length];
-        String[] resultBinaryString = new String[arr1.length];
         for (int i = 0; i < arr1.length; i++) {
-            arr1BinaryString[i] = Integer.toBinaryString(arr1[i]);
-            arr2BinaryString[i] = Integer.toBinaryString(arr2[i]);
-            while (arr1BinaryString[i].length() < arr1.length){
-                arr1BinaryString[i] = "0" + arr1BinaryString[i];
+           String arr1Binary = Integer.toBinaryString(arr1[i]);
+           String arr2Binary = Integer.toBinaryString(arr2[i]);
+            while (arr1Binary.length() < arr1.length){
+                arr1Binary = "0" + arr1Binary;
             }
-            while (arr2BinaryString[i].length() < arr1.length){
-                arr2BinaryString[i] = "0" + arr2BinaryString[i];
+            while (arr2Binary.length() < arr1.length){
+                arr2Binary = "0" + arr2Binary;
             }
-            char[] charlist1 = arr1BinaryString[i].toCharArray();
-            char[] charlist2 = arr2BinaryString[i].toCharArray();
+            char[] charlist1 = arr1Binary.toCharArray();
+            char[] charlist2 = arr2Binary.toCharArray();
             answer[i] = "";
             for (int j = 0; j < charlist1.length; j++) {
                 if (charlist1[j] == '1' || charlist2[j] == '1') {
