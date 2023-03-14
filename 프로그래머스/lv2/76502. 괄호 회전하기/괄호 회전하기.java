cc25930c9deb7df746b927d3b8e554e731de1094
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-03-14 21:19:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 1.81 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,5 +1,4 @@
 import java.util.Stack;
-
 class Solution {
     public int solution(String s) {
         int answer = 0;
@@ -13,6 +12,7 @@ class Solution {
         }
         return answer;
     }
+    
     private boolean check(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
