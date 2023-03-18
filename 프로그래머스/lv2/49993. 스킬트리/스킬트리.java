6c8456c1e7673060dd885c7eeb1938926929008c
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-03-18 13:40:18 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,18 +1,36 @@
-import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
-        //ArrayList<String> skillTrees = new ArrayList<String>();
-        Iterator<String> it = skillTrees.iterator();
+        // 각 알파벳의 순서를 저장하기 위해 알파벳 갯수만큼 배열 선언
+        int[] skillCheck = new int[26];
 
-        while (it.hasNext()) {
-            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
-                it.remove();
+        // 1~26까지 각 알파벳 순서 등록
+        for (int i = 0; i < skill.length(); i++) {
+            char c = skill.charAt(i);
+            skillCheck[c - 65] = i + 1;
+        }
+
+        for (int i = 0; i < skill_trees.length; i++) {
+            // 지금까지 배운 현재 스킬 상황
+            int cnt = 1;
+            boolean isAble = true;
+            for (int j = 0; j < skill_trees[i].length(); j++) {
+                char c = skill_trees[i].charAt(j);
+
+                // 만약 지금 배울 스킬이 순서에 맞다면 cnt++
+                if (skillCheck[c - 65] == cnt) {
+                    cnt++;
+                }else if(skillCheck[c - 65] ==0){ // 순서 상관 없는 알파벳이면 넘기기
+                    continue;
+                }else{  // 순서 상관 있는 알파벳이지만, 현재 단계와 맞지 않으면 break
+                    isAble = false;
+                    break;
+                }
+            }
+            if(isAble){
+                answer++;
             }
         }
-        answer = skillTrees.size();
         return answer;
     }
 }
