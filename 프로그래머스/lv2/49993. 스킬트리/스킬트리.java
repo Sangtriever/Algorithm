9457c7f5d830dcfcd64bb4523fcfa0a2d30e15d0
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-03-18 13:34:15 +0900

    [level 2] Title: 스킬트리, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,30 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        int[] skillCheck = new int[26];
-        for (int i = 0; i < skill.length(); i++) {
-            char c = skill.charAt(i);
-            skillCheck[c - 65] = i + 1;
-        }
-        for (int i = 0; i < skill_trees.length; i++) {
-            int cnt = 1;
+        ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
+        //ArrayList<String> skillTrees = new ArrayList<String>();
+        Iterator<String> it = skillTrees.iterator();
 
-            for (int j = 0; j < skill_trees[i].length(); j++) {
-                char c = skill_trees[i].charAt(j);
-                if (skillCheck[c - 65] == cnt) {
-                    cnt++;
-                }else if(skillCheck[c - 65] ==0){
-
-                }else{
-                    break;
-                }
-                if(j == skill_trees[i].length()-1){
-                    answer++;
-                }
+        while (it.hasNext()) {
+            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
+                it.remove();
             }
         }
+        answer = skillTrees.size();
         return answer;
     }
 }
