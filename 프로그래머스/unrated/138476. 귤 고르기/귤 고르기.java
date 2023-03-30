2bbbcad08fc340061a1db30ca2c2bc32b9f496a2
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-03-30 20:11:59 +0900

    [unrated] Title: 귤 고르기, Time: 51.09 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -3,30 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-
-        Arrays.sort(tangerine);
-        ArrayList<Integer> list = new ArrayList<>();
-        int cnt = 1;
-        for (int i = 1; i < tangerine.length; i++) {
-            if(tangerine[i] == tangerine[i-1]){
-                cnt++;
-            }else{
-                list.add(cnt);
-                cnt = 1;
-            }
+        HashMap<Integer, Integer> hashMap = new LinkedHashMap<>();
+        for (int i = 0; i < tangerine.length; i++) {
+            hashMap.put(tangerine[i],hashMap.getOrDefault(tangerine[i],0)+1);
         }
-        list.add(cnt);
+        List<Integer> list = new ArrayList<>(hashMap.values());
         list.sort(Comparator.reverseOrder());
-        cnt = 0;
-
-        for (Integer integer : list) {
-            answer += integer;
-            cnt++;
-
-            if (k <= answer) {
-                return cnt;
+        for (int i = 0; i < list.size(); i++) {
+            k -= list.get(i);
+            if(k <= 0){
+                answer = i+1;
+                break;
             }
         }
-        return cnt;
+        return answer;
     }
 }
