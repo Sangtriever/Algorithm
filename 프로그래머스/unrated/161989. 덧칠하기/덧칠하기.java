9403bcc3e690e2a587c4d8bbb0e95160c1910e23
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-03-30 23:31:26 +0900

    [unrated] Title: 덧칠하기, Time: 0.82 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int line = 0;
+        int l = 0;
 
         for (int i = 0; i < section.length; i++) {
-            if(line <= section[i]){
-                line = section[i] + m;
+            if(l <= section[i]){
+                l = section[i] + m;
                 answer++;
             }
             
