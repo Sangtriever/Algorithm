7bee5ba6068297e0a9fbf78c2c93b61531ee358f
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-04-04 20:29:47 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        String s = String.valueOf(num);
+        for (int i = 0; i < s.length(); i++) {
+            if(Integer.parseInt(s.substring(i,i+1)) == k){
+                return i+1;
+            }
+        }
+        return answer;
+    }
+}
