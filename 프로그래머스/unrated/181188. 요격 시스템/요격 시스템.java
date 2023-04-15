e550d01b6d95672d52bd9cc8ccebcf1c65c618fb
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-04-15 22:13:35 +0900

    [unrated] Title: 요격 시스템, Time: 0.61 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+
+        Arrays.sort(targets, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[1] - o2[1];
+        });
+
+        int cut = -1;
+
+        for (int i = 0; i < targets.length; i++) {
+            int left = targets[i][0];
+            int right = targets[i][1];
+            if(left >= cut){
+                answer++;
+                cut = right;
+            }
+        }
+        return answer;
+    }
+}
