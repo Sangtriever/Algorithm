19efe1cba185207942bdaa578df726d2cfb7e711
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-05-10 23:10:57 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 14.43 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,19 +1,10 @@
-import java.util.Stack;
-
 class Solution {
     public String solution(String my_string, int[][] queries) {
-        for (int i = 0; i < queries.length; i++) {
-            int start = queries[i][0];
-            int end = queries[i][1]+1;
-            Stack<Character> stack = new Stack<>();
-            for (int j = start; j < end; j++) {
-                stack.push(my_string.charAt(j));
-            }
-            StringBuilder s = new StringBuilder();
-            while (!stack.isEmpty()){
-                s.append(stack.pop());
-            }
-            my_string = my_string.substring(0,start) + s.toString() + my_string.substring(end,my_string.length());
+        for (int[] query : queries) {
+            int start = query[0];
+            int end = query[1] + 1;
+            String s = new StringBuilder(my_string.substring(start, end)).reverse().toString();
+            my_string = my_string.substring(0, start) + s + my_string.substring(end, my_string.length());
         }
         return my_string;
     }
