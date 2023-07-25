8d8bede925e9a347fa9a9d1f533b03ac0303bd39
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-07-25 22:29:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.36 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,38 +2,41 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int[][] maps) {
-        int answer = 0;
-        int[][] visited = new int[maps.length][maps[0].length];
-        bfs(maps,visited);
-        answer = visited[maps.length-1][maps[0].length-1];
-        if(answer == 0){
-            answer = -1;
-        }
-        return answer;
-    }
-    public void bfs (int[][] maps, int[][] visited){
-        Queue<int[]> q = new LinkedList<>();
-        int[] dx = {0,0,1,-1};
-        int[] dy = {-1,1,0,0};
-        visited[0][0] = 1;
-        int x = 0;
-        int y = 0;
-        q.add(new int[]{x,y});
+	// 4방향 체크용 x,y, 
+	int[] mx = { 1, 0, -1, 0 };
+	int[] my = { 0, -1, 0, 1 };
+	// maps의 크기와 같은 visited 2차 배열 생성(해당 자리의 가장 빠르게 도달할 수 있는 값 저장용)
+	int[][] visited;
 
-        while (!q.isEmpty()){
-            int[] now = q.poll();
-            for (int i = 0; i < 4; i++) {
-                x = now[0] + dx[i];
-                y = now[1] + dy[i];
+	public int solution(int[][] maps) {
+		visited = new int[maps.length][maps[0].length];
+		bfs(maps);
+		
+		if (visited[maps.length - 1][maps[0].length - 1] == 0) return -1;
+		return visited[maps.length - 1][maps[0].length - 1];
+	}
 
-                if(x < maps.length && x >= 0 && y < maps[0].length && y >= 0){
-                    if(visited[x][y] == 0 && maps[x][y] == 1){
-                        visited[x][y] = visited[now[0]][now[1]] + 1;
-                        q.add(new int[]{x,y});
-                    }
-                }
-            }
-        }
-    }
+	public void bfs(int[][] maps) {
+		// bfs를 위한 q 생성 <좌표값>
+		Queue<int[]> q = new LinkedList<>();
+		int[] now = { 0, 0 }; // <시작 좌표값 설정>
+		q.add(new int[] { 0, 0 });
+		visited[0][0] = 1; // 첫번째 자리는 1로 시작
+		
+		// while문을 돌며 
+		while (!q.isEmpty()) {
+			// now => 현재 좌표 즉, q에 넣어뒀던 값을 꺼내서 확인
+			now = q.poll();
+			// 본인 위치 기준 동남서북 순서로 확인해보며 갈 수 있는 방향이면 q에 추가해주며 , 해당 자리의 visited에 +1 
+			for (int i = 0; i < 4; i++) {
+				int x = now[0] + mx[i];
+				int y = now[1] + my[i];
+				if (x > maps.length - 1 || y > maps[0].length - 1 || x < 0 || y < 0 || maps[x][y] == 0
+						|| visited[x][y] != 0)
+					continue;
+				q.add(new int[] { x, y });
+				visited[x][y] = visited[now[0]][now[1]] + 1;
+			}
+		}
+	}
 }
