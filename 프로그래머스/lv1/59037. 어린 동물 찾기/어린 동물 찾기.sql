a54c041ca564b3916968220813680de30b71d980
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-08-07 12:48:02 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/lv1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
-SELECT animal_id, name
-from animal_ins
-where intake_condition != 'aged'
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION != 'Aged'
+ORDER BY ANIMAL_ID
