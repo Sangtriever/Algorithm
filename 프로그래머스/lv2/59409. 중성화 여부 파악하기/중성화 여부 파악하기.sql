f9ea12fa871531781431b9fcc1f1a23f6fe5ccc6
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-08-17 13:49:22 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,8 +1,5 @@
-SELECT ANIMAL_ID, NAME, 
-CASE
-WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
-WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
-ELSE 'X'
-end 
+SELECT ANIMAL_ID, NAME, CASE WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+                             WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+                             ELSE 'X' END AS 중성화
 FROM ANIMAL_INS
-order by animal_id
+ORDER BY ANIMAL_ID
