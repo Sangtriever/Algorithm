9be8aa7910ca3f8926066b6553c9c701580f562a
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-08-29 14:43:03 +0900

    [level 2] Title: 숫자의 표현, Time: 0.91 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,15 +2,13 @@ class Solution {
     public int solution(int n) {
         int answer = 1;
         for (int i = 1; i < n/2+1; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += j;
-                if(sum > n){
-                    break;
-                }else if(sum == n){
+            int sum = i;
+            for (int j = i+1; sum <= n; j++) {
+                if(sum == n){
                     answer++;
                     break;
                 }
+                sum += j;
             }
         }
         return answer;
