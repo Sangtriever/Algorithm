cc9f832e46c663a069bdfa617b454232f03e90cc
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2023-10-05 18:18:27 +0900

    [level 2] Title: N개의 최소공배수, Time: 2.38 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,19 +1,22 @@
 import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arr) {
-        int highNum = arr[arr.length-1];
-        int cnt = highNum;
-        roop : while (true){
-            highNum += cnt;
+        int answer = 0;
+        Arrays.sort(arr);
+        int maxInt = arr[arr.length-1];
+        int idx = 0;
+
+        loop:
+        while (true){
+            answer = maxInt * ++idx;
             for (int i = 0; i < arr.length-1; i++) {
-                if(highNum % arr[i] != 0){
-                    break;
-                }
-                if(i == arr.length-2){
-                    break roop;
+                if(answer % arr[i] != 0){
+                    continue loop;
                 }
             }
+            break;
         }
-        return highNum;
+        return answer;
     }
 }
