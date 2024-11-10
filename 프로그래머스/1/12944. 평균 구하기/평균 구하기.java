bd8682c5c6859218bd307d2f0fa69cafa6d1ac36
Sangtriever <109057365+Sangtriever@users.noreply.github.com>
2024-11-10 22:29:30 +0900

    [level 1] Title: 평균 구하기, Time: 3.69 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,10 +1,7 @@
 import java.util.*;
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        for (int j : arr) {
-            answer += j;
-        }
-        return answer/arr.length;
+               return Arrays.stream(arr).average().orElse(0);
+
     }
 }
